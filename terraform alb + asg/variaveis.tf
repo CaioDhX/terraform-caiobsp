9e59f32c7d8ce33bb8e13736e9d7ae7042f65846
CaioDhX <105319207+CaioDhX@users.noreply.github.com>
2025-04-14 09:54:29 -0300

    Create variaveis.tf

---
+++ terraform alb + asg/variaveis.tf
@@ -0,0 +1,11 @@
+variable "regiao" {
+    description = "Regiao da subnet"
+    type = string
+    default = "us-eats-1a"
+}
+
+variable "tipo" {
+    description = "tipo da ec2"
+    type = string
+    default = "t2.micro"
+}
